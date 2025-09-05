32a4229c1d7ae44a7752f950680ef92b23430d20
Connor Garey <connorgarey1999@gmail.com>
2025-09-05 23:56:42 +0100

    Moved to an app manager system with init update and cleanup functions

---
+++ src/application/app-manager.zig
@@ -0,0 +1,54 @@
+const Window = @import("../system/window.zig").Window;
+const Raylib = @import("../cimport/raylib.zig").Raylib;
+const Renderer = @import("../system/renderer.zig").Renderer;
+const LuaState = @import("../system/lua-instance.zig").LuaInstance;
+const Color = @import("../theming/colors.zig");
+const TextRender = @import("../system/text-rendering.zig");
+const Audio = @import("../system/audio.zig").Audio;
+
+pub const AppManager = struct {
+    lua_instance: LuaState = undefined,
+    audio_manager: Audio = undefined,
+    window: Window = undefined,
+    background_color: Raylib.Color = undefined,
+    
+    pub fn initApp(self: *AppManager) !void {
+        try self.lua_instance.initLua();
+
+        Window.initWindow(
+            @intFromFloat(try self.lua_instance.readGlobalFromFile("scripts/config.lua", "Width")), 
+            @intFromFloat(try self.lua_instance.readGlobalFromFile("scripts/config.lua", "Height")), 
+            "Mscales GUI");
+
+        Audio.init();
+        self.audio_manager.sample_rate = @intFromFloat(try self.lua_instance.readGlobalFromFile(
+                "scripts/config.lua", 
+                "SampleRate"));
+        self.audio_manager.sample_size = @intFromFloat(try self.lua_instance.readGlobalFromFile(
+                "scripts/config.lua", 
+                "SampleSize"));
+        self.audio_manager.channels = @intFromFloat(try self.lua_instance.readGlobalFromFile(
+                "scripts/config.lua", 
+                "Channels"));
+
+        self.background_color = .{ 
+            .a = 255,
+            .r = @intFromFloat(try self.lua_instance.readGlobalFromFile("scripts/config.lua", "BackgroundR")),
+            .g = @intFromFloat(try self.lua_instance.readGlobalFromFile("scripts/config.lua", "BackgroundG")),
+            .b = @intFromFloat(try self.lua_instance.readGlobalFromFile("scripts/config.lua", "BackgroundB")),
+        };
+
+
+    }
+    
+    pub fn updateApp(self: *AppManager) void {
+        Renderer.beginDraw();
+        Renderer.clearBackground(self.background_color);
+        Renderer.endDrawing();
+    }
+    
+    pub fn cleanupApp(self: *AppManager) void {
+        self.lua_instance.closeLua();
+        Window.closeWindow();
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,51 +1,14 @@
-const std = @import("std");
-const Window = @import("system/window.zig").Window;
 const Raylib = @import("cimport/raylib.zig").Raylib;
-const Renderer = @import("system/renderer.zig").Renderer;
-const LuaState = @import("system/lua-instance.zig").LuaInstance;
-const Color = @import("theming/colors.zig");
-const TextRender = @import("system/text-rendering.zig");
-const Audio = @import("system/audio.zig").Audio;
-
-const MusicScale = @import("application/music-scales.zig");
-
-const Vec2 = @import("util/vector2.zig");
+const AppManager = @import("application/app-manager.zig").AppManager;
 
 pub fn main() !void {
-    var lua_instance: LuaState = .{};
-    try lua_instance.initLua();
-    defer lua_instance.closeLua();
 
-    const screen_resolution: Vec2 .vectorTwo(c_int) = .{
-        .x = @intFromFloat(try lua_instance.readGlobalFromFile("scripts/config.lua", "Width")),
-        .y = @intFromFloat(try lua_instance.readGlobalFromFile("scripts/config.lua", "Height")),
-    };
-
-    Window.initWindow(screen_resolution.x, screen_resolution.y, "Mscales GUI");
-    defer Window.closeWindow();
-
-    const background_color: Raylib.Color = Color.createCustomColor(
-        @intFromFloat(try lua_instance.readGlobalFromFile("scripts/config.lua", "BackgroundR")), 
-        @intFromFloat(try lua_instance.readGlobalFromFile("scripts/config.lua", "BackgroundG")), 
-        @intFromFloat(try lua_instance.readGlobalFromFile("scripts/config.lua", "BackgroundB")), 
-    );
-
-    Audio.init();
-    var application_audio = Audio {
-        .sample_rate = @intFromFloat(try lua_instance.readGlobalFromFile("scripts/config.lua", "SampleRate")),
-        .sample_size = @intFromFloat(try lua_instance.readGlobalFromFile("scripts/config.lua", "SampleSize")),
-        .channels = @intFromFloat(try lua_instance.readGlobalFromFile("scripts/config.lua", "Channels")),
-    };
-    const audio_stream = application_audio.loadAudioStream();
+    var app_manager: AppManager = undefined;
+    try app_manager.initApp();
+    defer app_manager.cleanupApp();
 
     while (!Raylib.WindowShouldClose()) {
-        Raylib.PlayAudioStream(audio_stream);
-
-        Renderer.beginDraw(); 
-        Renderer.clearBackground(background_color); 
-        TextRender.TextRendering.drawTextToFixedPosition(MusicScale.CMajor.title, 32, 32, TextRender.ScreenPositions.TopCentre, Color.MiddleGrey);
-        TextRender.TextRendering.drawTextToFixedPosition(MusicScale.CMajor.scale_type, 16, 64, TextRender.ScreenPositions.TopCentre, Color.MiddleGrey);
-        Renderer.endDrawing();
+        app_manager.updateApp();
     }
 }
 
