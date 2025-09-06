1c1592b1ef24bd2477f8fdf6992b19c0e3875fea
Connor Garey <connorgarey1999@gmail.com>
2025-09-06 12:43:50 +0100

    Re arranged close so they are in backward order

--- src/application/app-manager.zig
+++ src/application/app-manager.zig
@@ -35,7 +35,7 @@ pub const AppManager = struct {
     
     pub fn cleanupApp(self: *AppManager) void {
         Audio.closeAudio();
-        self.lua_instance.closeLua();
         Window.closeWindow();
+        self.lua_instance.closeLua();
     }
 };
